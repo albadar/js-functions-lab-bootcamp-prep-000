6ffa4bc9b71c9bb381d8ca01cb3826888547c0af
Albara Darwish <albara.darwish@gmail.com>
2018-07-26 16:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
+<<<<<<< HEAD
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
@@ -14,3 +15,12 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
+=======
+//function happyHolidaysTo(name) {
+  //return 'Happy holidays, ' + name + '!'
+//}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+>>>>>>> b8f9b2fce4b0c802968955157fd912352d766ff7
